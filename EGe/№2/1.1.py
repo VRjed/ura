90a007040637f0479accb6cf42c3c00114d6140e
VRjed <vavav4781@gmail.com>
2023-11-24 13:52:29 +1100

    123

--- EGe/№2/1.1.py
+++ EGe/№2/1.1.py
@@ -1,7 +1,7 @@
-#((x → y ) ∧ (y → w)) ∨ (z ≡ ( x ∨ y)).
 for x in range(2):
     for y in range(2):
         for z in range(2):
             for w in range(2):
                 a = ((x <= y ) and (y <= w)) or (z == ( x or y))
                 print (x, y , z, w , a)
+#((x → y ) ∧ (y → w)) ∨ (z ≡ ( x ∨ y)).
