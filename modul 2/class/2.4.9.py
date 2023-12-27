008cf935f3a609e679aee555f3a566a6c4809095
VRjed <vavav4781@gmail.com>
2023-12-27 17:11:05 +1100

    11

--- modul 2/class/2.4.9.py
+++ modul 2/class/2.4.9.py
@@ -1,5 +1,8 @@
 a = int(input()) 
 s = 0
-for i in str(a):
-    if int(i) == 2 * a - 1:
-        s +=1
+for i in range(a + 1):
+    if i == 1:
+        s = 1
+    else:
+        s = s + (i + (i - 1))
+print(s)
