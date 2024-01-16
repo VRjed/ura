3fbc7ed4e32e1ca4d9733c5dd0acd86ac6532784
VRjed <vavav4781@gmail.com>
2024-01-16 14:50:08 +1100

    123

--- modul 2/final/2.1.2.py
+++ modul 2/final/2.1.2.py
@@ -2,12 +2,15 @@ a = int(input())
 b = int(input())
 c = int(input())
 h = 1
-for i in range(a+1):
-    g = a - b + c
-    a = g 
-    if a < b:
-        break
-    else:
-        h += 1
+if a < b:
+    h = 0
+else:
+    for i in range(a+1):
+        g = a - b + c
+        a = g 
+        if a < b:
+            break
+        else:
+            h += 1
 print(h)
 
---
+++ modul 2/final/2.1.3.py
@@ -0,0 +1,25 @@
+bull = 10000
+cow = 5000
+calf = 500
+money = 100000
+units = 100
+countbull = 0
+countcow = 0
+countcalf = 0
+while money - bull >= 0:
+    countbull += 1
+    money = money - bull
+if money != 0:
+    while money - cow >= 0:
+        countcow += 1
+        money = money - cow
+if money != 0:
+    while money - calf >= 0:
+        countcalf += 1
+        money = money - calf
+if money != 0:
+    print(-1)
+    print(-1)
+    print(-1)
+else:
+    print(countbull,countcow,countcalf)
