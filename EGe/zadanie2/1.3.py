eadcd609ea2599047f1d1fe0e86c0b654f343445
VRjed <vavav4781@gmail.com>
2024-02-18 15:03:35 +1100

    1334

--- EGe/zadanie2/1.3.py
+++ EGe/zadanie2/1.3.py
@@ -1,8 +1,8 @@
-#(x ≡ (y → z)) ∧ (¬w → (x ≡ y))
+#((x → y ) ≡ (z → w)) ∨ (x ∧ w).
 for x in range(2):
     for y in range(2):
         for z in range(2):
             for w in range(2):
-                a = (x == (y <= z)) and ((not(w)) <= (x == y))
+                a = ((x <= y ) == (z <= w)) or (x and w)
                 print(x, y , z ,w , a)
-            #(x ≡ (y → z)) ∧ (¬w → (x ≡ y))
+            #((x → y ) ≡ (z → w)) ∨ (x ∧ w).
